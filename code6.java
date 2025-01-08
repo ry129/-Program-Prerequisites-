190b4e021fc2af08e2400a7ce51c342d27de6f9d
ry129 <0111cs211146.riya@gmail.com>
2025-01-08 10:37:24 +0530

    Perimeter of a Rectangle

---
+++ code6.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class simple {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int p =sc.nextInt();
+		double r=sc.nextDouble();
+		double t = sc.nextDouble();
+		System.out.println((p*r*t)/100);
+	}
+}
